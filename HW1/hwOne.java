6f77fab6cdd5367c6fd7fa9fd0d56e9f77c73d8d
Thanh Doan <tcdoan@cpp.edu>
2020-01-26 22:04:44 -0800

    modified Goodbye,...

--- HW1/hwOne.java
+++ HW1/hwOne.java
@@ -1,6 +1,6 @@
 public class hwOne {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, my name is Thanh Doan");
+		System.out.println("Goodbye, I finished my CS3700 HW1");
 	}
 }
